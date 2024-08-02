c3e07d71356d3efe13e6101bbb319391589de396
ismail-coder-fix <ndismail007@gmail.com>
2024-08-02 22:03:58 +0200

    Added logo

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="nav">
-    <router-link to="/" ><h1>makria</h1></router-link>
+    <router-link to="/" >
+      <div class="logo">
+        <img src="../assets/logo.png" alt="Logo" />
+        <h1>MAKRIA</h1>
+      </div>
+    </router-link>
     <div class="nav-links">
         <router-link to="/about" >about</router-link>
         <router-link to="/about" >services</router-link>
         <router-link to="/projects">projects</router-link>
-        <router-link class="uniq" to="/contact">talkto us</router-link>
+        <router-link class="uniq" to="/contact">talk to us</router-link>
     </div>
   </div>
 </template>
